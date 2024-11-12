fa82880abbc7da78e03c6b52f8b08a6a518ed96a
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-11-12 16:49:05 +0700

    updated

---
+++ src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpring.java
@@ -0,0 +1,17 @@
+package com.in28minutes.learn_spring_framework;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App02HelloWorldSpring {
+	public static void main(String[] args) {
+		//1: Launch a Spring context
+		var context = 
+				new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+		
+		//2: Config things that we want Spring manage - @Configuration
+		var bean = context.getBean("name"); 
+		System.out.println(bean.toString());
+		
+		context.close();
+	}
+}
